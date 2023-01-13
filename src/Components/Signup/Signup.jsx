0cff71f2a66c167712e52ba66433e48fa9f4e5d5
Samora Laurent <laurentsamora6@gmail.com>
2023-01-13 22:44:36 +0300

    created signup page

--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <>
+      <div className="form-input">
+        <div className="form">
+          <form>
+            <input type="text" placeholder="Name" name="name" required />
+            <input type="email" placeholder="Email" name="email" required />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              required
+            />
+            <input
+              type="password"
+              placeholder=" Confirm Password"
+              name="password"
+              required
+            />
+            <button>Signup</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Signup;
