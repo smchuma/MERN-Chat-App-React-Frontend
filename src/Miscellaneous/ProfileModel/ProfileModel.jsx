ed6990b8ca5d16becf1177b12dd98312d3aae21d
Samora Laurent <laurentsamora6@gmail.com>
2023-01-29 22:53:34 +0300

    worked on the profile model

--- src/Miscellaneous/ProfileModel/ProfileModel.jsx
+++ src/Miscellaneous/ProfileModel/ProfileModel.jsx
@@ -48,31 +48,37 @@ const ProfileModel = ({ user, children }) => {
             >
               <Image
                 borderRadius="full"
-                boxSize="150px"
+                boxSize="180px"
                 alt={user.name}
                 src={user.pic}
               />
             </Box>
-            <div className="titles">
-              <div className="icon">
-                <i className="fa-solid fa-user"></i>
+            <Box>
+              <div className="titles">
+                <div className="text-content">
+                  <i className="fa-solid fa-user"></i>
+                  <div className="text">
+                    <h3>Name</h3>
+                    <h1>{user.name}</h1>
+                  </div>
+                </div>
+                <div className="icon2">
+                  <i className="fa-solid fa-pen"></i>
+                </div>
               </div>
-              <div className="text">
-                <h2>Name</h2>
-                <h1>{user.name}</h1>
+              <div className="titles">
+                <div className="text-content">
+                  <i class="fa-solid fa-envelope"></i>
+                  <div className="text">
+                    <h3>Email</h3>
+                    <h1>{user.email}</h1>
+                  </div>
+                </div>
+                <div className="icon2">
+                  <i className="fa-solid fa-pen"></i>
+                </div>
               </div>
-              <i className="fa-solid fa-pen"></i>
-            </div>
-            <div className="titles">
-              <div className="icon">
-                <i class="fa-solid fa-envelope"></i>
-              </div>
-              <div className="text">
-                <h2>Email</h2>
-                <h1>{user.email}</h1>
-              </div>
-              <i className="fa-solid fa-pen"></i>
-            </div>
+            </Box>
           </ModalBody>
           <ModalFooter>
             <Button onClick={onClose}>Close</Button>
